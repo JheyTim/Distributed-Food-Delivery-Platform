ed76c38178dc02e824c7e6968661b5a31f0d7ed6
Timothy Jhey Sarmiento <timothyjhey@gmail.com>
2024-09-29 05:22:14 +0800

    Commenting OAuth implementation

--- auth-service/server.js
+++ auth-service/server.js
@@ -1,9 +1,9 @@
 require('dotenv').config();
-require('./config/passport');
+// require('./config/passport');
 
 const express = require('express');
-const passport = require('passport');
-const jwt = require('jsonwebtoken')
+// const passport = require('passport');
+// const jwt = require('jsonwebtoken');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/authRoutes');
 
@@ -16,23 +16,23 @@ app.use(express.json());
 app.use('/auth', authRoutes);
 
 // Google OAuth
-app.get(
-  '/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] })
-);
+// app.get(
+//   '/auth/google',
+//   passport.authenticate('google', { scope: ['profile', 'email'] })
+// );
 
-app.get(
-  '/auth/google/callback',
-  passport.authenticate('google', { session: false }),
-  (req, res) => {
-    // Send JWT token after Google login
-    const payload = { user: { id: req.user.id, role: req.user.role } };
-    const token = jwt.sign(payload, process.env.JWT_SECRET, {
-      expiresIn: '1h',
-    });
-    res.json({ token });
-  }
-);
+// app.get(
+//   '/auth/google/callback',
+//   passport.authenticate('google', { session: false }),
+//   (req, res) => {
+//     // Send JWT token after Google login
+//     const payload = { user: { id: req.user.id, role: req.user.role } };
+//     const token = jwt.sign(payload, process.env.JWT_SECRET, {
+//       expiresIn: '1h',
+//     });
+//     res.json({ token });
+//   }
+// );
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
